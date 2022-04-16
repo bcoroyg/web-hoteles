5a69f691c7aa1a2b96161114ae011359b65719d9
Brandon Coroy <becgonzalez25@gmail.com>
2022-04-16 17:06:42 -0600

    Automatización de tareas Gulp - browser-sync y watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,30 @@ const nodeSass = require('node-sass');
 const gulpSass = require('gulp-sass');
 const sass = gulpSass(nodeSass);
 
+const browserSync = require("browser-sync");
+
 gulp.task('sass', function() {
   return gulp.src('./assets/css/*.scss')
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./assets/css"));
-})
+});
+
+gulp.task("sass:watch", function(){
+    gulp.watch("./assets/css/*.scss", gulp.series("sass"))
+});
+
+gulp.task("browser-sync", function () {
+    const files = [
+      "./*.html",
+      "./assets/css/*.css",
+      "./assets/img/*.{png, jpg, gif}",
+      "/assets/js/*.js",
+    ];
+    browserSync.init(files, {
+      server: {
+        baseDir: "./",
+      },
+    });
+  });
+
+  gulp.task("default", gulp.parallel("sass:watch","browser-sync"));
